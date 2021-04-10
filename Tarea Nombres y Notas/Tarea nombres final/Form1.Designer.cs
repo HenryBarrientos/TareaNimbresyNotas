259128430ef1c199c89e06e248b97ff42d5f1df2
Henry Barrientos Flores <henryflo1389@gmail.com>
2021-04-10 10:16:09 -0600

    HOLA

---
+++ Tarea Nombres y Notas/Tarea nombres final/Form1.Designer.cs
@@ -0,0 +1,395 @@
+﻿
+namespace ejemplos_case8__arreglos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonArreglo = new System.Windows.Forms.Button();
+            this.listBoxResultado = new System.Windows.Forms.ListBox();
+            this.buttonCargar = new System.Windows.Forms.Button();
+            this.textBoxContenido = new System.Windows.Forms.TextBox();
+            this.buttonNombres = new System.Windows.Forms.Button();
+            this.listBoxNombres = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.parcial1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.parcial2 = new System.Windows.Forms.ListBox();
+            this.parcial3 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonArreglo
+            // 
+            this.buttonArreglo.Location = new System.Drawing.Point(13, 136);
+            this.buttonArreglo.Name = "buttonArreglo";
+            this.buttonArreglo.Size = new System.Drawing.Size(75, 23);
+            this.buttonArreglo.TabIndex = 0;
+            this.buttonArreglo.Text = "Arreglo";
+            this.buttonArreglo.UseVisualStyleBackColor = true;
+            this.buttonArreglo.Click += new System.EventHandler(this.buttonArreglo_Click);
+            // 
+            // listBoxResultado
+            // 
+            this.listBoxResultado.FormattingEnabled = true;
+            this.listBoxResultado.Location = new System.Drawing.Point(12, 175);
+            this.listBoxResultado.Name = "listBoxResultado";
+            this.listBoxResultado.Size = new System.Drawing.Size(145, 173);
+            this.listBoxResultado.TabIndex = 1;
+            this.listBoxResultado.SelectedIndexChanged += new System.EventHandler(this.listBoxResultado_SelectedIndexChanged);
+            // 
+            // buttonCargar
+            // 
+            this.buttonCargar.Location = new System.Drawing.Point(12, 12);
+            this.buttonCargar.Name = "buttonCargar";
+            this.buttonCargar.Size = new System.Drawing.Size(75, 23);
+            this.buttonCargar.TabIndex = 2;
+            this.buttonCargar.Text = "Cargar archivo";
+            this.buttonCargar.UseVisualStyleBackColor = true;
+            this.buttonCargar.Click += new System.EventHandler(this.buttonCargar_Click);
+            // 
+            // textBoxContenido
+            // 
+            this.textBoxContenido.Location = new System.Drawing.Point(103, 14);
+            this.textBoxContenido.Multiline = true;
+            this.textBoxContenido.Name = "textBoxContenido";
+            this.textBoxContenido.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxContenido.Size = new System.Drawing.Size(370, 104);
+            this.textBoxContenido.TabIndex = 3;
+            // 
+            // buttonNombres
+            // 
+            this.buttonNombres.Location = new System.Drawing.Point(12, 41);
+            this.buttonNombres.Name = "buttonNombres";
+            this.buttonNombres.Size = new System.Drawing.Size(75, 23);
+            this.buttonNombres.TabIndex = 4;
+            this.buttonNombres.Text = "Nombres";
+            this.buttonNombres.UseVisualStyleBackColor = true;
+            this.buttonNombres.Click += new System.EventHandler(this.buttonNombres_Click);
+            // 
+            // listBoxNombres
+            // 
+            this.listBoxNombres.FormattingEnabled = true;
+            this.listBoxNombres.Location = new System.Drawing.Point(498, 12);
+            this.listBoxNombres.Name = "listBoxNombres";
+            this.listBoxNombres.Size = new System.Drawing.Size(460, 108);
+            this.listBoxNombres.TabIndex = 5;
+            this.listBoxNombres.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Notas";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // parcial1
+            // 
+            this.parcial1.FormattingEnabled = true;
+            this.parcial1.Location = new System.Drawing.Point(187, 149);
+            this.parcial1.Name = "parcial1";
+            this.parcial1.Size = new System.Drawing.Size(127, 199);
+            this.parcial1.TabIndex = 7;
+            this.parcial1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Resultados";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(214, 380);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(214, 406);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(214, 432);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 380);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Parcial 1= nota mas alta";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 406);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Parcial 1= nota mas baja";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 432);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Parcial 1= Promedio de notas";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(564, 432);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 18;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(564, 406);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 17;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(564, 380);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 16;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(904, 432);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 21;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(904, 406);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 20;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(904, 380);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(360, 432);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 13);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Parcial 2 = Promedio de notas ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(360, 406);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(126, 13);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Parcial 2 = nota mas baja";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(360, 380);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(120, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Parcial 2= nota mas alta";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(700, 380);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(120, 13);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "Parcial 3= nota mas alta";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(700, 406);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(123, 13);
+            this.label8.TabIndex = 26;
+            this.label8.Text = "Parcial 3= nota mas baja";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(700, 432);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(144, 13);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Parcial 3= promedio de notas";
+            // 
+            // parcial2
+            // 
+            this.parcial2.FormattingEnabled = true;
+            this.parcial2.Location = new System.Drawing.Point(528, 149);
+            this.parcial2.Name = "parcial2";
+            this.parcial2.Size = new System.Drawing.Size(136, 199);
+            this.parcial2.TabIndex = 28;
+            // 
+            // parcial3
+            // 
+            this.parcial3.FormattingEnabled = true;
+            this.parcial3.Location = new System.Drawing.Point(868, 149);
+            this.parcial3.Name = "parcial3";
+            this.parcial3.Size = new System.Drawing.Size(136, 199);
+            this.parcial3.TabIndex = 29;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 588);
+            this.Controls.Add(this.parcial3);
+            this.Controls.Add(this.parcial2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.parcial1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBoxNombres);
+            this.Controls.Add(this.buttonNombres);
+            this.Controls.Add(this.textBoxContenido);
+            this.Controls.Add(this.buttonCargar);
+            this.Controls.Add(this.listBoxResultado);
+            this.Controls.Add(this.buttonArreglo);
+            this.Name = "Form1";
+            this.Text = "Cargar Archivo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonArreglo;
+        private System.Windows.Forms.ListBox listBoxResultado;
+        private System.Windows.Forms.Button buttonCargar;
+        private System.Windows.Forms.TextBox textBoxContenido;
+        private System.Windows.Forms.Button buttonNombres;
+        private System.Windows.Forms.ListBox listBoxNombres;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox parcial1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ListBox parcial2;
+        private System.Windows.Forms.ListBox parcial3;
+    }
+}
+
---
+++ Tarea Nombres y Notas/Tarea nombres final/Form1.cs
@@ -0,0 +1,157 @@
+﻿using ejemplos_case8__arreglos.clases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejemplos_case8__arreglos
+{
+    public partial class Form1 : Form
+    {
+
+        private string[] ArregloNotas;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonArreglo_Click(object sender, EventArgs e)
+        {
+            int[] arreglo = new int[5]; //creando el arreglo
+            arreglo[0] = 10;//asignando valor a la posicion 0 del arreglo
+            arreglo[1] = 8;
+            arreglo[2] = 16;
+            arreglo[3] = 36;
+            arreglo[4] = 1;
+
+            ClsArreglos ObjArreglo = new ClsArreglos(arreglo);
+            int[] resultado = ObjArreglo.MetodoBurbuja();
+
+            for (int indice = 0; indice < resultado.Length; indice++)//esta diciendo que vaya de 0 hasta la ultima posicion del arreglo o vector
+            {
+                //  listBoxResultado.Items.Add(arreglo[indice]);
+                listBoxResultado.Items.Add($"valor= {resultado[indice]} posicion={indice} ");
+
+            }
+
+            //podemos usar un for o un foreach y es lo mismo
+
+            // foreach(int r in arreglo)//estamos diciendo que interactue r en arreglo que es el nombre del vector y el int es porque el vector es tipo int
+            //{
+            //   listBoxResultado.Items.Add(arreglo[indice]);
+            //}
+
+        }
+
+        private void buttonCargar_Click(object sender, EventArgs e)
+        {
+            ClsArchivos ar = new ClsArchivos();
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Selecciona tu archivo plano ";
+            ofd.InitialDirectory = @"C:\Users\FAMILIA BARRIENTOS\Downloads\archivoplano.csv";
+            ofd.Filter = "archivoplano(*.csv)|*.csv";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                var archivo = ofd.FileName; //devuelve el nombre y el lugar del archivo
+                string resultado = ar.LeerTodoArchivo(archivo);
+
+                ArregloNotas = ar.LeerArchivo(archivo); //retorna arreglo
+
+                textBoxContenido.Text = resultado;
+            }
+
+        }
+
+        private void buttonNombres_Click(object sender, EventArgs e)
+        {
+            int contador = 0;
+            string[] nombres = new string[ArregloNotas.Length - 1];
+            foreach (string linea in ArregloNotas)
+            {
+                if (contador != 0)
+                {
+                    string[] datos = linea.Split(';'); //devuelve una posicion arreglo por cada punto y cma que divida los nombres
+                    nombres[contador - 1] = datos[1];
+                }
+
+                contador++;
+            }
+            ClsArreglos arreglonombres = new ClsArreglos(nombres);
+            string[] resultados = arreglonombres.MetodoBurbujaNombres();
+            for (int indice = 0; indice < resultados.Length; indice++)//esta diciendo que vaya de 0 hasta la ultima posicion del arreglo o vector
+            {
+                listBoxNombres.Items.Add(resultados[indice]); //devuelve la posicion uno del arreglo datos que es el nombre
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (string linea in ArregloNotas)
+            {
+
+                string[] datos = linea.Split(';');
+                parcial1.Items.Add(datos[2]);
+                parcial2.Items.Add(datos[3]);
+                parcial3.Items.Add(datos[4]);
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            
+            textBox1.Text = parcial1.Items.Cast<string>().Max(a => Convert.ToInt32(a)).ToString();         
+            textBox2.Text = parcial1.Items.Cast<string>().Min(a => Convert.ToInt32(a)).ToString();         
+            textBox3.Text = parcial1.Items.Cast<string>().Average(a => Convert.ToInt32(a)).ToString();                                          
+            textBox4.Text = parcial2.Items.Cast<string>().Max(a => Convert.ToInt32(a)).ToString();           
+            textBox5.Text = parcial2.Items.Cast<string>().Min(a => Convert.ToInt32(a)).ToString();           
+            textBox6.Text = parcial2.Items.Cast<string>().Average(a => Convert.ToInt32(a)).ToString();
+            textBox7.Text = parcial3.Items.Cast<string>().Max(a => Convert.ToInt32(a)).ToString();
+            textBox8.Text = parcial3.Items.Cast<string>().Min(a => Convert.ToInt32(a)).ToString();
+            textBox9.Text = parcial3.Items.Cast<string>().Average(a => Convert.ToInt32(a)).ToString();
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBoxResultado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
